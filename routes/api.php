4a33fa933ed9c590b426c8e40df924509d1393be
Pradeep Rajapaksha <pradeep.rajapsksha@zincat.net>
2024-12-21 03:33:33 +0530

    user auth apis

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::post('/auth/register', [\App\Http\Controllers\Api\AuthController::class, 'register']);
+Route::post('/auth/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/auth/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    // Logout route
+    Route::post('/auth/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout']);
 });
